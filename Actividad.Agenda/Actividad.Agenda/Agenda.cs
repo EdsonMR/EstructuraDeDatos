68c93ad839c19717b80a02f37ee30997b9d53024
Edson Moreno <edsonmorenoruiz@gmail.com>
2018-04-16 18:04:53 -0500

    Actividad Agenda

---
+++ Actividad.Agenda/Actividad.Agenda/Agenda.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad.Agenda
+{
+    class Agenda
+    {
+        public int tamaño = 4;
+        private Contacto[] agenda = new Contacto[5];
+        public int contador = 0;
+
+        public void agregar(Contacto conti)
+        {
+            agenda[contador] = conti;
+            contador++;
+        }
+
+        public Contacto buscar(string telefono)
+        {
+            for (int x = 0; x < tamaño; x++)
+            if (agenda[x].Telefono == telefono) return agenda[x];
+
+            return null;
+        }
+
+        public void eliminar(string telefono)
+        {
+            for (int x = 0; x < tamaño; x++)
+                if (agenda[x].Telefono == telefono)
+                    for (int y = x; y < tamaño - 1; y++) agenda[y] = agenda[y + 1];
+
+            agenda[tamaño - 1] = null;
+            contador--;
+        }
+
+        public string listar()
+        {
+            string cadena = "";
+            for (int x = 0; x < contador; x++)
+            {
+                cadena += agenda[x].Nombre +  " " + agenda[x].APaterno + " " + agenda[x].AMaterno + " , " + agenda[x].Telefono + " , " + agenda[x].Edad + " , " + agenda[x].Email + Environment.NewLine;
+            }
+
+            return cadena;
+        }
+
+        public void insertar(int pos, Contacto con)
+        {
+            for (int x = tamaño - 1; x > (pos - 1); x--) agenda[x] = agenda[x - 1];
+            agenda[pos - 1] = con;
+            contador++;
+        }
+    }
+}
